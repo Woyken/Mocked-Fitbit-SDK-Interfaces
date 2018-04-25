665f280cc60dc4e2f0e96503c1b563c6c29484d7
Karolis Užkuraitis <karolis.uzkuraitis+github@gmail.com>
2018-04-25 23:35:35 +0300

    cbor Float to class

--- MockedFitBitSDKInterfaces/CommonApi/cbor.d.ts
+++ MockedFitBitSDKInterfaces/CommonApi/cbor.d.ts
@@ -1,5 +1,8 @@
 declare module "cbor" {
     export function decode(cbor: ArrayBuffer | ArrayBufferView): any;
     export function encode(value: any): ArrayBuffer;
-    export function Float(value: number): undefined;
+    class Float {
+        constructor(value: number);
+        value: number;
+    }
 }
